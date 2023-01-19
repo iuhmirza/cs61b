f0f74680ecfb614583e10e89d441fdd939f1b636
Ibtisam Mirza <iuhmirza@gmail.com>
2023-01-19 13:18:52 +0000

    Complete lab2

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,29 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+
+    @Test
+    public void testSquarePrimesFirstSix() {
+        IntList lst = IntList.of(2, 3, 5, 7, 11, 13);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 9 -> 25 -> 49 -> 121 -> 169", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void testSquarePrimesZero() {
+        IntList lst = IntList.of(0, 0, 0, 0, 0, 0);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("0 -> 0 -> 0 -> 0 -> 0 -> 0", lst.toString());
+        assertFalse(changed);
+    }
+
+    @Test
+    public void testSquarePrimesComplex() {
+        IntList lst = IntList.of(0, 1, 10, 100, 2, -1);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("0 -> 1 -> 10 -> 100 -> 4 -> -1", lst.toString());
+        assertTrue(changed);
+    }
 }
